555accdf9cb79187d8e3f571162c0d2c3749c0de
Yauheni <jenka26122@gmail.com>
2022-03-19 00:04:40 +0300

    add page translation

--- js/script.js
+++ js/script.js
@@ -11,18 +11,18 @@ window.onclick = function (event) {
     let dropdowns = document.getElementsByClassName("dropdown-content");
 
     let i;
-  
-    for (i = 0; i < dropdowns.length; i++) {  
+
+    for (i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
       }
     }
-   
   }
   if (!event.target.matches(".bar1,.bar2,.bar3,.dropbtn")) {
     let dropdownss = document.getElementsByClassName("dropbtn");
-    let j; for (j = 0; j < dropdownss.length; j++) {
+    let j;
+    for (j = 0; j < dropdownss.length; j++) {
       let openDropdown = dropdownss[j];
       if (openDropdown.classList.contains("change")) {
         openDropdown.classList.remove("change");
@@ -50,13 +50,37 @@ portfolioBtn.forEach((btn) => {
 
 //cache
 
-const seasons = ['winter', 'spring', 'summer', 'autumn'];
-function preloadImages(){
-  seasons.forEach((season)=>{
-    for (let i=1;i<=6;i++){
-      const img=new Image();
-      img.src=`./images/${season}/${i}.jpg`
+const seasons = ["winter", "spring", "summer", "autumn"];
+function preloadImages() {
+  seasons.forEach((season) => {
+    for (let i = 1; i <= 6; i++) {
+      const img = new Image();
+      img.src = `./images/${season}/${i}.jpg`;
     }
-  })
+  });
 }
-preloadImages()
+preloadImages();
+//translate page
+import i18Obj from "./translate.js";
+
+const lang = document.querySelectorAll(".header__menu-language span");
+
+lang.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    lang.forEach((el) => {
+      el.classList.remove("check");
+    });
+    elem.classList.toggle("check");
+
+    !lang[0].className.includes("check")
+      ? getTranslate("ru")
+      : getTranslate("en");
+  });
+});
+
+function getTranslate(language) {
+  let result = document.querySelectorAll("[data-i18]");
+  result.forEach((element) => {
+    element.textContent = i18Obj[language][element.dataset.i18];
+  });
+}
